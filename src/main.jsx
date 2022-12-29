a66b8a6599a618faebe434f00ec3bb96bf9c1e13
Feri Ramdani <ferii.ramdani007@gmail.com>
2022-12-29 17:33:21 +0700

    add mobile detect

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { isBrowser } from 'react-device-detect'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {
+      (!isBrowser) ? (
+        <div>
+          <h5 className='device-detect'>Sorry, this content available only on Desktop</h5>
+        </div>
+      ) :
+      (
+        <App />
+      )
+    }
   </React.StrictMode>
 )
